4ee897b30006c9ddce308cb234270e1cb4336944
ntnngan210 <92852032+ntnngan210@users.noreply.github.com>
2022-12-22 23:49:12 +0700

    first commit

---
+++ Views/introduce/contact.php
@@ -0,0 +1,53 @@
+
+<!-- pages-title-start -->
+<div class="pages-title section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="pages-title-text text-center">
+					<h2>Thông tin liên hệ</h2>
+					<ul class="text-left">
+						<li><a href="?act=home">Trang chủ</a></li>
+						<li><span> // </span>Liên hệ</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- pages-title-end -->
+<!-- contact content section start -->
+<div class="pages contact-page section-padding">
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-sm-10 col-text-center">
+				<div class="contact-details">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-map-marker"></i>
+								<p>180 Đường Cao Lỗ,</p>
+								<p>Trường đại học Công Nghệ Sài Gòn</p>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-phone"></i>
+								<p>028 3850 5520</p>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-email"></i>
+								<p>dangadmin@gmail.com</p>
+								<p>thienadmin@gmail.com</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- contact content section end -->
+
